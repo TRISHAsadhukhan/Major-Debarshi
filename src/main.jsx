ae74a4f54347367101364305f8c4fc7995c4ad56
TRISHAsadhukhan <sadhukhantrisha1@gmail.com>
2024-05-07 12:43:20 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Room from './Room.jsx'
+import PopUp from './PopUp.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Room />
+  </React.StrictMode>,
+)
